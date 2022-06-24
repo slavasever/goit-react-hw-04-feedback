1c48b4d264ab0792133b7f108d77bcca9dc5d741
Viacheslav Severyn <sever_2008@ukr.net>
2022-06-24 13:56:31 +0300

    bug_fixed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const options = ['good', 'neutral', 'bad'];
 
   const countTotalFeedback = () => {
     return good + neutral + bad;
@@ -40,7 +39,10 @@ function App() {
   return (
     <>
       <Section title="Please leave feedback">
-        <FeedbackOptions options={options} handleClick={handleClick} />
+        <FeedbackOptions
+          options={Object.keys({ good, neutral, bad })}
+          handleClick={handleClick}
+        />
       </Section>
 
       {countTotalFeedback() === 0 ? (
